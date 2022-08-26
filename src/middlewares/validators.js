64edf7d7793ca443b83956af1aed984412df85ae
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2022-08-26 12:33:57 -0300

    feat: validador do body da requisição de criação de usuário

--- src/middlewares/validators.js
+++ src/middlewares/validators.js
@@ -2,15 +2,29 @@ const Joi = require('joi');
 const { CustomError } = require('../errors');
 
 const validators = {
-  bodyLogin: async (req, res, next) => {
+  bodyLogin: async (req, _res, next) => {
     const schema = Joi.object({
-      email: Joi.string().min(1).required(),
+      email: Joi.string().email().min(1).required(),
       password: Joi.string().min(6).required(),
     });
 
     const { error } = schema.validate(req.body);
 
     if (error) throw new CustomError(400, 'Some required fields are missing');
+
+    next();
+  },
+  bodyUser: async (req, _res, next) => {
+    const schema = Joi.object({
+      displayName: Joi.string().min(8).required(),
+      email: Joi.string().email().min(1).required(),
+      password: Joi.string().min(6).required(),
+      image: Joi.string().required(),
+    });
+
+    const { error } = schema.validate(req.body);
+
+    if (error) throw new CustomError(400, error.details[0].message);
 
     next();
   },
